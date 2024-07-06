f67b2e3ebcb3a314b65d5a922704414271277c69
vpinti <vittorio.pinti@gmail.com>
2024-07-06 16:45:52 +0200

    feat: form home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
+import { PatientForm } from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-    return <h1 className="text-3xl underline">Hello</h1>;
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div className="flex h-screen max-h-screen">
+            <section className="remove-scrollbar container my-auto">
+                <div className="sub-container max-w-[496px]">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        height={1000}
+                        width={1000}
+                        alt="patient"
+                        className="mb-12 h-10"
+                    />
+                    <PatientForm />
+                    <div className="text-14-regoular mt-20 flex justify-between">
+                        <p className="justify-items-end text-dark-600 xl:text-left">
+                            © {currentYear} CarePluse
+                        </p>
+                        <Link href="/?admin=true" className="text-green-500">
+                            Admin
+                        </Link>
+                    </div>
+                </div>
+            </section>
+            <Image
+                src="/assets/images/onboarding-img.png"
+                height={1000}
+                width={1000}
+                alt="patient"
+                className="side-img max-w-[50%]"
+            />
+        </div>
+    );
 }
